07eef78e767cbea4e2594a993a71e13f1e5ec0ad
André Noordzij <noordzij.am@gmail.com>
2025-05-12 20:26:08 +0200

    Base Memoryboard

--- SPMemory/Controls/MemoryBoard.xaml.cs
+++ SPMemory/Controls/MemoryBoard.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SPMemory.Models;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,14 +8,28 @@ namespace SPMemory.Controls
 	/// <summary>
 	/// Interaction logic for MemoryBoard.xaml
 	/// </summary>
-	public partial class MemoryBoard : UserControl
+	public partial class MemoryBoard : UserControl, INotifyPropertyChanged
 	{
 		public MemoryBoard()
 		{
 			InitializeComponent();
+			//DataContext = this;
 		}
 
-		public static DependencyProperty MemoryCardsProperty = DependencyProperty.Register(nameof(MemoryCard), typeof(List<MemoryCard>), typeof(MemoryBoard), new PropertyMetadata() { PropertyChangedCallback = MemoryCardsChangedCallback } );
+		public static DependencyProperty MemoryCardsProperty = DependencyProperty.Register(nameof(MemoryCards), typeof(List<MemoryCard>), typeof(MemoryBoard), new PropertyMetadata() { PropertyChangedCallback = MemoryCardsChangedCallback } );
+		public static DependencyProperty NumCardsHorizontalProperty = DependencyProperty.Register(nameof(NumCardsHorizontal), typeof(int), typeof(MemoryBoard), new PropertyMetadata() { PropertyChangedCallback = NumCardsHorizontalChangedCallback });
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private static void MemoryCardsChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((MemoryBoard)d).RaisePropertyChanged(nameof(PlayingGrid));
+		}
+		
+		private static void NumCardsHorizontalChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((MemoryBoard)d).RaisePropertyChanged(nameof(PlayingGrid));
+		}
 
 		public List<MemoryCard> MemoryCards
 		{
@@ -22,9 +37,30 @@ namespace SPMemory.Controls
 			set => SetValue(MemoryCardsProperty, value);
 		}
 
-		public static void MemoryCardsChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		public int NumCardsHorizontal
 		{
+			get => (int)GetValue(NumCardsHorizontalProperty);
+			set => SetValue(NumCardsHorizontalProperty, value);
+		}
 
+		public IEnumerable<IEnumerable<MemoryCard>> PlayingGrid
+		{
+			get
+			{
+				if(MemoryCards == null)
+				{
+					yield break;
+				}
+				for(int i = 0; i < MemoryCards.Count; i+= NumCardsHorizontal)
+				{
+					yield return MemoryCards.Skip(i).Take(NumCardsHorizontal);
+				}
+			}
+		}
+
+		protected void RaisePropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
---
+++ SPMemory/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SPMemory.Models;
+
+namespace SPMemory.ViewModels
+{
+	public class MainWindowViewModel
+	{
+		public List<MemoryCard> Test { get; set; } = Enumerable.Repeat(new MemoryCard(), 35).ToList();
+
+		public int NumCardsHorizontal { get; set; } = 5;
+	}
+}
